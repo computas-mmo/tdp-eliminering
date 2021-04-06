72236c37855f88f5a526a3183ca4d1abbdb318e2
Magnus Moen <mmo@computas.com>
2021-04-06 13:27:14 +0200

    testmodul skriver til fil, men med feil tegnsett

--- testmodul/testmodul.py
+++ testmodul/testmodul.py
@@ -7,9 +7,17 @@ NAME = 'ac:name'
 
 with open("page.html") as page:
     soup = BeautifulSoup(page, "html.parser")
-    # print(soup.prettify())
+    print("open page.html")
+    diff = False
     for macro in soup.find_all(name=MACRO, attrs={NAME: "tdp-fragment"}):
-        print("hi\n")
-        print(macro.prettify())
+        print("do replacement of tdp-fragment")
+        # print(macro.prettify())
         macro[NAME] = 'multiexcerpt'
         macro.find(PARAMETER, attrs={NAME: 'key'})[NAME] = 'name'
+        diff = True
+
+    if diff:
+        print("Diff found, trying to update new_page.html")
+        open("new_page.html", "a").write(soup.decode())
+    else:
+        print("No diff found, page is unchanged.")
